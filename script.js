829fae33a9c3e3672fd245aaa5a18e95f3af96d9
Ruslan <ruslankandiba88@gmail.com>
2020-05-31 12:08:32 +0400

    first task

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 "use strict";
 
+const numbersOfFilms = +prompt("Skolko filmov vi uje posmotreli?", "");
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt("Odin iz poslednix filmov?", ""),
+      b = prompt("na skolko ocenice ego?", ""),
+      c = prompt("Odin iz poslednix filmov?", ""),
+      d = prompt("na skolko ocenice ego?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
